28248b68581bf9f3f8b8815dbf2ea24c705bc475
ParaHuang <paradox041@gmail.com>
2025-08-12 15:31:20 +0800

    area table script

---
+++ area_sql_script.sql
@@ -0,0 +1,300 @@
+-- states（without state_id）
+INSERT INTO area (city, state_id) VALUES
+('Alabama', NULL),
+('Alaska', NULL),
+('Arizona', NULL),
+('Arkansas', NULL),
+('California', NULL),
+('Colorado', NULL),
+('Connecticut', NULL),
+('Delaware', NULL),
+('Florida', NULL),
+('Georgia', NULL),
+('Hawaii', NULL),
+('Idaho', NULL),
+('Illinois', NULL),
+('Indiana', NULL),
+('Iowa', NULL),
+('Kansas', NULL),
+('Kentucky', NULL),
+('Louisiana', NULL),
+('Maine', NULL),
+('Maryland', NULL),
+('Massachusetts', NULL),
+('Michigan', NULL),
+('Minnesota', NULL),
+('Mississippi', NULL),
+('Missouri', NULL),
+('Montana', NULL),
+('Nebraska', NULL),
+('Nevada', NULL),
+('New Hampshire', NULL),
+('New Jersey', NULL),
+('New Mexico', NULL),
+('New York', NULL),
+('North Carolina', NULL),
+('North Dakota', NULL),
+('Ohio', NULL),
+('Oklahoma', NULL),
+('Oregon', NULL),
+('Pennsylvania', NULL),
+('Rhode Island', NULL),
+('South Carolina', NULL),
+('South Dakota', NULL),
+('Tennessee', NULL),
+('Texas', NULL),
+('Utah', NULL),
+('Vermont', NULL),
+('Virginia', NULL),
+('Washington', NULL),
+('Washington D.C.', NULL),
+('West Virginia', NULL),
+('Wisconsin', NULL),
+('Wyoming', NULL);
+
+
+-- cities
+
+-- Alabama (id=1)
+INSERT INTO area (city, state_id) VALUES
+('Birmingham', 1),
+('Montgomery', 1),
+('Alabama (other)', 1);
+
+-- Alaska (id=2)
+INSERT INTO area (city, state_id) VALUES
+('Anchorage', 2),
+('Alaska (other)', 2);
+
+-- Arizona (id=3)
+INSERT INTO area (city, state_id) VALUES
+('Phoenix', 3),
+('Tucson', 3),
+('Arizona (other)', 3);
+
+-- Arkansas (id=4) no city
+
+-- California (id=5)
+INSERT INTO area (city, state_id) VALUES
+('Bakersfield', 5),
+('Fresno', 5),
+('Los Angeles', 5),
+('Modesto', 5),
+('Oxnard', 5),
+('Riverside', 5),
+('Sacramento', 5),
+('San Bernardino', 5),
+('San Diego', 5),
+('San Francisco', 5),
+('California (other)', 5);
+
+-- Colorado (id=6)
+INSERT INTO area (city, state_id) VALUES
+('Colorado Springs', 6),
+('Denver', 6),
+('Colorado (other)', 6);
+
+-- Connecticut (id=7) no city
+
+-- Delaware (id=8) no city
+
+-- Florida (id=9)
+INSERT INTO area (city, state_id) VALUES
+('Fort Lauderdale', 9),
+('Jacksonville', 9),
+('Miami', 9),
+('Orlando', 9),
+('Tampa', 9),
+('West Palm Beach', 9),
+('Florida (other)', 9);
+
+-- Georgia (id=10)
+INSERT INTO area (city, state_id) VALUES
+('Atlanta', 10),
+('Georgia (other)', 10);
+
+-- Hawaii (id=11)
+INSERT INTO area (city, state_id) VALUES
+('Honolulu', 11),
+('Hawaii (other)', 11);
+
+-- Idaho (id=12)
+INSERT INTO area (city, state_id) VALUES
+('Boise', 12),
+('Idaho (other)', 12);
+
+-- Illinois (id=13)
+INSERT INTO area (city, state_id) VALUES
+('Chicago', 13),
+('Illinois (other)', 13);
+
+-- Indiana (id=14)
+INSERT INTO area (city, state_id) VALUES
+('Fort Wayne', 14),
+('Indianapolis', 14),
+('Indiana (other)', 14);
+
+-- Iowa (id=15)
+INSERT INTO area (city, state_id) VALUES
+('Des Moines', 15),
+('Iowa (other)', 15);
+
+-- Kansas (id=16)
+INSERT INTO area (city, state_id) VALUES
+('Wichita', 16),
+('Kansas (other)', 16);
+
+-- Kentucky (id=17)
+INSERT INTO area (city, state_id) VALUES
+('Lexington', 17),
+('Louisville', 17),
+('Kentucky (other)', 17);
+
+-- Louisiana (id=18)
+INSERT INTO area (city, state_id) VALUES
+('Baton Rouge', 18),
+('New Orleans', 18),
+('Louisiana (other)', 18);
+
+-- Maine (id=19) no city
+
+-- Maryland (id=20)
+INSERT INTO area (city, state_id) VALUES
+('Baltimore', 20),
+('Maryland (other)', 20);
+
+-- Massachusetts (id=21)
+INSERT INTO area (city, state_id) VALUES
+('Boston', 21),
+('Massachusetts (other)', 21);
+
+-- Michigan (id=22)
+INSERT INTO area (city, state_id) VALUES
+('Detroit', 22),
+('Michigan (other)', 22);
+
+-- Minnesota (id=23)
+INSERT INTO area (city, state_id) VALUES
+('Minneapolis', 23),
+('Minnesota (other)', 23);
+
+-- Mississippi (id=24) no city
+
+-- Missouri (id=25)
+INSERT INTO area (city, state_id) VALUES
+('Kansas City', 25),
+('St. Louis', 25),
+('Missouri (other)', 25);
+
+-- Montana (id=26) no city
+
+-- Nebraska (id=27) no city
+
+-- Nevada (id=28)
+INSERT INTO area (city, state_id) VALUES
+('Las Vegas', 28),
+('Reno', 28),
+('Nevada (other)', 28);
+
+-- New Hampshire (id=29) no city
+
+-- New Jersey (id=30) no city
+
+-- New Mexico (id=31)
+INSERT INTO area (city, state_id) VALUES
+('Albuquerque', 31),
+('New Mexico (other)', 31);
+
+-- New York (id=32)
+INSERT INTO area (city, state_id) VALUES
+('Buffalo', 32),
+('New York City', 32),
+('Rochester', 32),
+('New York State (other)', 32);
+
+-- North Carolina (id=33)
+INSERT INTO area (city, state_id) VALUES
+('Charlotte', 33),
+('Fayetteville', 33),
+('Greensboro', 33),
+('Raleigh', 33),
+('North Carolina (other)', 33);
+
+-- North Dakota (id=34) no city
+
+-- Ohio (id=35)
+INSERT INTO area (city, state_id) VALUES
+('Cincinnati', 35),
+('Cleveland', 35),
+('Columbus', 35),
+('Toledo', 35),
+('Ohio (other)', 35);
+
+-- Oklahoma (id=36)
+INSERT INTO area (city, state_id) VALUES
+('Oklahoma City', 36),
+('Tulsa', 36),
+('Oklahoma (other)', 36);
+
+-- Oregon (id=37)
+INSERT INTO area (city, state_id) VALUES
+('Portland', 37),
+('Oregon (other)', 37);
+
+-- Pennsylvania (id=38)
+INSERT INTO area (city, state_id) VALUES
+('Philadelphia', 38),
+('Pennsylvania (other)', 38);
+
+-- Rhode Island (id=39) no city
+
+-- South Carolina (id=40) no city
+
+-- South Dakota (id=41) no city
+
+-- Tennessee (id=42)
+INSERT INTO area (city, state_id) VALUES
+('Memphis', 42),
+('Tennessee (other)', 42);
+
+-- Texas (id=43)
+INSERT INTO area (city, state_id) VALUES
+('Austin', 43),
+('Dallas', 43),
+('El Paso', 43),
+('Fort Worth', 43),
+('Houston', 43),
+('Laredo', 43),
+('Lubbock', 43),
+('San Antonio', 43),
+('Texas (other)', 43);
+
+-- Utah (id=44) no city
+
+-- Vermont (id=45) no city
+
+-- Virginia (id=46)
+INSERT INTO area (city, state_id) VALUES
+('Richmond', 46),
+('Virginia Beach', 46),
+('Virginia (other)', 46);
+
+-- Washington (id=47)
+INSERT INTO area (city, state_id) VALUES
+('Seattle', 47),
+('Spokane', 47),
+('Washington (other)', 47);
+
+-- Washington D.C. (id=48) no city
+
+-- West Virginia (id=49) no city
+
+-- Wisconsin (id=50)
+INSERT INTO area (city, state_id) VALUES
+('Madison', 50),
+('Milwaukee', 50),
+('Wisconsin (other)', 50);
+
+-- Wyoming (id=51) no city
+
+   
