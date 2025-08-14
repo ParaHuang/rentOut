9ef101401a2d74028b4f38b25a0ca0a5b80507b3
ParaHuang <paradox041@gmail.com>
2025-08-14 20:45:43 +0800

    new version

---
+++ rentout_uk/rentout_uk/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rentout_uk project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rentout_uk.settings')
+
+application = get_asgi_application()
