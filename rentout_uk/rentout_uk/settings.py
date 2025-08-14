5b3d6b3fcb461f737d37a02a978f628ddcd0d5a7
ParaHuang <paradox041@gmail.com>
2025-08-14 21:05:26 +0800

    new version

--- rentout_uk/rentout_uk/settings.py
+++ rentout_uk/rentout_uk/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'users',
+    'listings',
+    'roommates',
+    'transactions',
+    'messaging',
+    'reviews',
 ]
 
 MIDDLEWARE = [
